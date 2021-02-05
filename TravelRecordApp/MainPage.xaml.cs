e722f2623c478e8f2375cc4aedf7cf026fff0e85
simonjv01 <sjvargas01@gmail.com>
2021-02-04 22:32:00 -0500

    first commit

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+
+    }
+}
